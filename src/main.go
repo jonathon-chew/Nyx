662cbf5514ac3ece01de1b8305d8d0e7d1f88f85
Jonathon Chew <>
2025-04-21 00:54:22 +0100

    Adding the location to save the picture to, to be the users Picture file on a MAC or home directory in a folder called Pictures

--- src/main.go
+++ src/main.go
@@ -1,115 +1,122 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"strings"
-	"bufio"
+  "encoding/json"
+  "fmt"
+  "io"
+  "log"
+  "net/http"
+  "os"
+  "strings"
+  "bufio"
+  "path/filepath"
 )
 
 type Response struct{
-	Copyright string
-	Date string
-	Explanation string
-	Hdurl string
-	Media_type string
-	Service_version string
-	Title string
-	Url string
+  Copyright string
+  Date string
+  Explanation string
+  Hdurl string
+  Media_type string
+  Service_version string
+  Title string
+  Url string
 }
 
 func downloadImage(fileName string, response *http.Response){
 
-	outFile, err := os.Create(fileName)
-	
-	if err != nil{
-		log.Fatal(err)
-	}
+  outFile, err := os.Create(fileName)
+  
+  if err != nil{
+    log.Fatal(err)
+  }
 
-	defer outFile.Close()	
-	
-	_, err = io.Copy(outFile, response.Body)
-	if err != nil{
-		log.Fatal(err)
-	}
-	
-	fmt.Println("Image successfully downloaded and saved as '%s'", fileName)
+  defer outFile.Close()  
+  
+  _, err = io.Copy(outFile, response.Body)
+  if err != nil{
+    log.Fatal(err)
+  }
+  
+  fmt.Printf("Image successfully downloaded and saved as %s", fileName)
 
 }
 
 func main(){
 
-	file, err := os.Open("../scripts/key.txt")
-	if err != nil{
-		log.Fatal("Failed to open file: %s", err)
-	}
+  file, err := os.Open("./scripts/key.txt")
+  if err != nil{
+    log.Fatal("Failed to open file: %s", err)
+  }
 
-	defer file.Close()
+  defer file.Close()
 
-	// Create a scanner to read the file line by line
-	scanner := bufio.NewScanner(file)
+  // Create a scanner to read the file line by line
+  scanner := bufio.NewScanner(file)
 
-	lineNumber := 0
-	var key string
+  lineNumber := 0
+  var key string
 
-	// Read lines one by one
-	for scanner.Scan() {
-		lineNumber++
-		if lineNumber == 5 { // Get the 5th line
-			key = scanner.Text()
-			break // Stop reading after the 5th line
-		}
-	}
+  // Read lines one by one
+  for scanner.Scan() {
+    lineNumber++
+    if lineNumber == 5 { // Get the 5th line
+      key = scanner.Text()
+      break // Stop reading after the 5th line
+    }
+  }
 
-	// Handle possible scanning errors
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
+  // Handle possible scanning errors
+  if err := scanner.Err(); err != nil {
+    fmt.Println("Error reading file:", err)
+    return
+  }
 
-	var url string = strings.TrimSpace(key)
-	
-	resp, err := http.Get(url)
+  var url string = strings.TrimSpace(key)
+  
+  resp, err := http.Get(url)
 
-	if err != nil{
-		log.Fatal(err)
-	}
+  if err != nil{
+    log.Fatal(err)
+  }
 
-	defer resp.Body.Close()
+  defer resp.Body.Close()
 
-	// Read the response body
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error reading response body: %v", err)
-	}
+  // Read the response body
+  body, err := io.ReadAll(resp.Body)
+  if err != nil {
+    log.Fatalf("Error reading response body: %v", err)
+  }
 
-	// Parse JSON into struct
-	var todaysResponse Response 
-	if err := json.Unmarshal(body, &todaysResponse); err != nil {
-		log.Fatalf("Error unmarshalling JSON: %v", err)
-	}
+  // Parse JSON into struct
+  var todaysResponse Response 
+  if err := json.Unmarshal(body, &todaysResponse); err != nil {
+    log.Fatalf("Error unmarshalling JSON: %v", err)
+  }
 
-	if todaysResponse.Media_type == "image"{
+  if todaysResponse.Media_type == "image"{
 
-		imageData, err := http.Get(todaysResponse.Url)
-		if err != nil{
-			log.Fatal(err)
-		}
+    imageData, err := http.Get(todaysResponse.Url)
+    if err != nil{
+      log.Fatal(err)
+    }
 
-		var fileName string
+    var fileName string
 
-		extension := strings.Split(todaysResponse.Url, ".")
-		fileName = todaysResponse.Title + "." + extension[len(extension) - 1]
+    extension := strings.Split(todaysResponse.Url, ".")
+    homeDir, err := os.UserHomeDir()
+    if err != nil {
+        log.Fatal("Unable to determine user home directory:", err)
+    }
 
-		downloadImage(fileName, imageData)
+    fileName = filepath.Join(homeDir, "Picturess")
+    fileName = fileName + todaysResponse.Title + "." + extension[len(extension) - 1]
 
-		// Print parsed data
-		fmt.Printf("Title: %s\nMedia Type: %s\nURL: %s\n", todaysResponse.Title, todaysResponse.Media_type, todaysResponse.Url)
+    downloadImage(fileName, imageData)
 
-	}
+    // Print parsed data
+    fmt.Printf("Title: %s\nMedia Type: %s\nURL: %s\n", todaysResponse.Title, todaysResponse.Media_type, todaysResponse.Url)
+
+  }
 
 }
